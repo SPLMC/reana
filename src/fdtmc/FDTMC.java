4e1213896cd67958be852d5a7b7972bac9dcd523
Marcelo Oliveira <martins.oliveira.mo@gmail.com>
2017-09-29 18:56:27 -0300

    Refector getTransitionByActionName method
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>
    Signed-off-by: Marcelo Oliveira <martins.oliveira.mo@gmail.com>

--- src/fdtmc/FDTMC.java
+++ src/fdtmc/FDTMC.java
@@ -175,21 +175,30 @@ public class FDTMC {
 		}
 		return null;
 	}
+	
+	public Transition returnTransition(Iterator<List<Transition>> iteratorStateAdjacencies, String action) {
+		List<Transition> transitions = iteratorStateAdjacencies.next();
+
+		//Percorrer a lista de transicoes e comparar os labels das transicoes
+		Iterator <Transition> iteratorTransitions = transitions.iterator();
+		while (iteratorTransitions.hasNext()) {
+			Transition t = iteratorTransitions.next();
+			if (t.getActionName().equals(action))
+				return t;
+		}
+		return null;
+	}
 
 	public Transition getTransitionByActionName(String action) {
 		//para cada Lista de adjacencias de cada nodo
 		Collection<List<Transition>> stateAdjacencies = transitionSystem.values();
 		Iterator<List<Transition>> iteratorStateAdjacencies = stateAdjacencies.iterator();
+		Transition t;
 		while (iteratorStateAdjacencies.hasNext()) {
-			List<Transition> transitions = iteratorStateAdjacencies.next();
-
-			//Percorrer a lista de transicoes e comparar os labels das transicoes
-			Iterator <Transition> iteratorTransitions = transitions.iterator();
-			while (iteratorTransitions.hasNext()) {
-				Transition t = iteratorTransitions.next();
-				if (t.getActionName().equals(action))
-					return t;
-			}
+				
+			t = returnTransition(iteratorStateAdjacencies, action);
+			return t;
+				
 		}
 		return null;
 	}
