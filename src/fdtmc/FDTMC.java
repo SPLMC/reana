b42527a6ab83351ca5670b591c27fb05f02673cc
Daniel <danmoura17@gmail.com>
2017-09-29 20:47:08 -0300

    Implementing extract method techinique

--- src/fdtmc/FDTMC.java
+++ src/fdtmc/FDTMC.java
@@ -206,13 +206,18 @@ public class FDTMC {
 			List<Transition> transitionList = this.transitionSystem.get(temp);
 			if (transitionList != null) {
 				Iterator <Transition> itTransitions = transitionList.iterator();
-				while (itTransitions.hasNext()) {
-					Transition t = itTransitions.next();
-					msg += temp.getVariableName() + "=" + temp.getIndex() + ((temp.getLabel() != null) ? "(" + temp.getLabel() + ")" : "") +
-							" --- " + t.getActionName() + " / " + t.getProbability() +
-							" ---> " + t.getTarget().getVariableName() + "=" + t.getTarget().getIndex() + ((t.getTarget().getLabel() != null) ? "(" + t.getTarget().getLabel() + ")" : "") + "\n";
-				}
+				msg = creteStateMessage(msg, temp, itTransitions);
 			}
+		}
+		return msg;
+	}
+
+	private String creteStateMessage(String msg, State temp, Iterator<Transition> itTransitions) {
+		while (itTransitions.hasNext()) {
+			Transition t = itTransitions.next();
+			msg += temp.getVariableName() + "=" + temp.getIndex() + ((temp.getLabel() != null) ? "(" + temp.getLabel() + ")" : "") +
+					" --- " + t.getActionName() + " / " + t.getProbability() +
+					" ---> " + t.getTarget().getVariableName() + "=" + t.getTarget().getIndex() + ((t.getTarget().getLabel() != null) ? "(" + t.getTarget().getLabel() + ")" : "") + "\n";
 		}
 		return msg;
 	}
